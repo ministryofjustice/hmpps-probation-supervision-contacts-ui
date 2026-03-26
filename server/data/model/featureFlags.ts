7a077a0caf90fcbd6c685f7a413ade1c5761de04
Harsh Vasudev <harsh.vasudev@justice.gov.uk>
2026-03-26 14:15:09 +0000

    MAN-2214 - tidied up flags that are not used

--- server/data/model/featureFlags.ts
+++ server/data/model/featureFlags.ts
@@ -1,7 +1,5 @@
 /* eslint-disable lines-between-class-members */
 export class FeatureFlags {
   [index: string]: boolean
-  enableContactLog?: boolean = undefined
-  enableCreateContact?: boolean = undefined
   searchContactsByCategory?: boolean = undefined
 }
