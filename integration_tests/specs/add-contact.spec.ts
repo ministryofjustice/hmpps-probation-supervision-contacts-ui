b27e43811fb42f2f8c9653b7e8313905c4d8cf25
Akshay-MOJ <Akshay.Ramchurn@justice.gov.uk>
2026-04-10 09:16:40 +0100

    revert test changes

--- integration_tests/specs/add-contact.spec.ts
+++ integration_tests/specs/add-contact.spec.ts
@@ -84,18 +84,6 @@ test('navigates to the activity log page with a success banner after filling add
   await addContactPage.selectSensitivityNo()
   await addContactPage.selectAlertResponsibleOfficerNo()
 
-  page.on('request', request => {
-    // eslint-disable-next-line no-console
-    console.log('### SUBMIT REQUEST ###', request.method(), request.url())
-  })
-
-  page.on('response', async response => {
-    if (response.status() >= 400) {
-      // eslint-disable-next-line no-console
-      console.log('### SUBMIT FAILED RESPONSE ###', response.status(), response.url())
-    }
-  })
-
   await addContactPage.clickContinue()
 
   await addContactPage.expectUrlToBe('case/X123456/activity-log?showSuccessBanner=true')
