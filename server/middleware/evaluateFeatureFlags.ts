0b519eac7030abb9d4e9727e4c96ab8d16b9906e
Aidan Filby <146082039+AidanFilby@users.noreply.github.com>
2026-04-27 09:59:41 +0100

    MTRSB-57 (#66)

--- server/middleware/evaluateFeatureFlags.ts
+++ server/middleware/evaluateFeatureFlags.ts
@@ -5,7 +5,7 @@ import FlagService from '../services/flagService'
 export default function evaluateFeatureFlags(flagService: FlagService): RequestHandler {
   return async (_req, res, next) => {
     try {
-      const flags = await flagService.getFlags()
+      const flags = await flagService.getFlags({ email: res.locals.user.email })
       if (flags) {
         res.locals.flags = flags
       } else {
