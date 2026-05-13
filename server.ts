43f7c56dfb1ed68e05f49b4c309062562947a99f
Ravindra Kumar <ravindrakumar.killamsetty@justice.gov.uk>
2026-05-13 10:31:08 +0100

    MTRSB-74: Add click analytics/ app-insights

--- server.ts
+++ server.ts
@@ -1,9 +1,27 @@
 // Require app insights before anything else to allow for instrumentation of bunyan and express
 import 'applicationinsights'
 
-import app from './server/index'
+import { app, metricsApp } from './server/index'
 import logger from './logger'
+import { buildAppInsightsClient, initialiseAppInsights } from './server/utils/azureAppInsights'
+import applicationInfoSupplier from './server/applicationInfo'
+
+const applicationInfo = applicationInfoSupplier()
+initialiseAppInsights()
+buildAppInsightsClient(applicationInfo)
+
+const validatePort = (port: number) => {
+  if (port >= 0 && port <= 65535) {
+    return port
+  }
+  throw new Error(`Port number out of range ${port}`)
+}
 
 app.listen(app.get('port'), () => {
   logger.info(`Server listening on port ${app.get('port')}`)
+})
+
+const metricsPort = validatePort(metricsApp.get('port'))
+metricsApp.listen(metricsPort, () => {
+  logger.info(`Metrics server listening on port ${metricsPort}`)
 })
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,10 @@
+import promClient from 'prom-client'
+import { createMetricsApp } from './monitoring/metricsApp'
 import createApp from './app'
 import { services } from './services'
 
+promClient.collectDefaultMetrics()
 const app = createApp(services())
+const metricsApp = createMetricsApp()
 
-export default app
+export { app, metricsApp }
