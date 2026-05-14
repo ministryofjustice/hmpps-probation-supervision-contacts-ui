e5f4de88fdcadbf0a1a84d28d50f4aefcf8fa7bd
Akshayreport <49643455+Akshayreport@users.noreply.github.com>
2026-05-14 16:38:52 +0100

    mock pages

--- server/controllers/updateContact.ts
+++ server/controllers/updateContact.ts
@@ -17,59 +17,60 @@ const getStringValue = (value: unknown): string => {
 const updateContactController = {
   getUpdateContact: (): RequestHandler => {
     return async (req, res, next) => {
-      try {
-        const { crn, contactId } = req.params as Record<string, string>
+      const { crn, contactId } = req.params as Record<string, string>
 
-        const { contact } = res.locals
+      const { contact } = res.locals
 
-        const displayName = contact.appointment?.displayName
+      const displayName = contact.appointment?.displayName
 
-        const isOutcome = !NoOutcomeContactTypeDetails.some(item => item.description === displayName)
+      const isOutcome = !NoOutcomeContactTypeDetails.some(item => item.description === displayName)
 
-        return res.render('pages/contacts/update-contact', {
-          crn,
-          contactId,
-          contact,
-          isOutcome,
-          csrfToken: res.locals.csrfToken,
-        })
-      } catch (error) {
-        return next(error)
-      }
+      return res.render('pages/contacts/update-contact', {
+        crn,
+        contactId,
+        contact,
+        isOutcome,
+        csrfToken: res.locals.csrfToken,
+      })
     }
   },
 
   postupdateContact: (masApiClient: MasApiClient): RequestHandler => {
     return async (req, res, next) => {
-      try {
-        const { crn, contactId } = req.params as Record<string, string>
-        const { username } = res.locals.user
+      const { crn, contactId } = req.params as Record<string, string>
+      const { username } = res.locals.user
 
-        const date = getStringValue(req.body?.date)
-        const time = getStringValue(req.body?.time)
-        let notes = getStringValue(req.body?.details)
-        const sensitivity = getStringValue(req.body?.sensitivity)
-        const contactType = res.locals.contact.appointment?.displayName
-        const existingNotes = res.locals.contact.appointment.appointmentNotes[0]?.note
-
-        const normaliseText = (value: string) => value.replace(/\r\n/g, '\n').trim()
-        if (normaliseText(existingNotes) === normaliseText(notes)) {
-          notes = ''
-        }
-        const isOutcome = !NoOutcomeContactTypeDetails.some(item => item.description === contactType)
-        const contactService = new ContactService(masApiClient)
-
-        const formattedDateandTime = toIsoDateTime(date, time)
-        const payload: UpdateContactWithNoOutcome = {
-          dateTime: formattedDateandTime,
-          notes: notes || '',
-          sensitiveFlag: sensitivity === 'Yes',
-        }
-        await contactService.updateContactWithNoOutcome(contactId, payload, username)
-        return res.redirect(`${config.manageProbationUrl}/case/${crn}/activity/${contactId}?showSuccessBanner=true`)
-      } catch (e) {
-        return next(e)
+      const date = getStringValue(req.body?.date)
+      const time = getStringValue(req.body?.time)
+      let notes = getStringValue(req.body?.details)
+      const sensitivity = getStringValue(req.body?.sensitivity)
+      const contactType = res.locals.contact.appointment?.displayName
+      const existingNotes = res.locals.contact.appointment.appointmentNotes[0]?.note
+      const normaliseText = (value: string) => value.replace(/\r\n/g, '\n').trim()
+      if (normaliseText(existingNotes) === normaliseText(notes)) {
+        notes = ''
       }
+      const isOutcome = !NoOutcomeContactTypeDetails.some(item => item.description === contactType)
+      const contactService = new ContactService(masApiClient)
+
+      const formattedDateandTime = toIsoDateTime(date, time)
+      const payload: UpdateContactWithNoOutcome = {
+        dateTime: formattedDateandTime,
+        notes: notes || '',
+        sensitiveFlag: sensitivity === 'Yes',
+      }
+      await contactService.updateContactWithNoOutcome(contactId, payload, username)
+      if (req.file) {
+        try {
+          await contactService.patchDocuments(crn, contactId.toString(), req.file, username)
+          return res.redirect(`${config.manageProbationUrl}/case/${crn}/activity/${contactId}?showSuccessBanner=true`)
+        } catch {
+          return res.redirect(
+            `${config.manageProbationUrl}/case/${crn}/activity/${contactId}?showSuccessBanner=true&uploadFailed=true`,
+          )
+        }
+      }
+      return res.redirect(`${config.manageProbationUrl}/case/${crn}/activity/${contactId}?showSuccessBanner=true`)
     }
   },
 }
