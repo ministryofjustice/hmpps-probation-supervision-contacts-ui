ea305216194312e3ab3c578abf982aff33ac5a52
Akshay-MOJ <Akshay.Ramchurn@justice.gov.uk>
2026-06-02 14:11:59 +0100

    new structure

--- integration_tests/specs/update-contact.spec.ts
+++ integration_tests/specs/update-contact.spec.ts
@@ -15,7 +15,7 @@ test.beforeEach(async ({ page }) => {
     masApi.stubGetSentences(),
     masApi.stubGetOverview(),
     masApi.stubGetContact(),
-    masApi.stubGetContactOutome(),
+    masApi.stubGetContactOutcome(),
   ])
   await login(page)
 })
