74a8034911e6f24472ccc3a697f6a27d92a7d8ca
Gerald Isabelle <gerald.isabelle@justice.gov.uk>
2026-06-12 10:30:26 +0100

    MAN-2710: remove unused type

--- server/data/model/outcomeContactTypes.ts
+++ server/data/model/outcomeContactTypes.ts
@@ -520,24 +520,4 @@ export const OutcomeContactTypeDetails: ContactTypeDetail[] = [
       },
     ],
   },
-  {
-    code: 'COUP',
-    description: 'Unplanned contact from person on probation',
-    sensitive: false,
-    relatesTo: ['Person', 'Pre-CJA Events', 'CJA/ORA Events'],
-    mandatoryOutcome: true,
-    outcomes: [
-      {
-        label: 'Failed to comply with other instruction',
-        value: 'ATFI',
-        systemLabel: 'Failed to Comply with other Instruction',
-      },
-      { label: 'No action required', value: 'CO22', systemLabel: 'No Action Required' },
-      {
-        label: 'Unacceptable behaviour',
-        value: 'UBHV',
-        systemLabel: 'Unacceptable Behaviour',
-      },
-    ],
-  },
 ]
